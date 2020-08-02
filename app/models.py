17fe5ed789567cdf850bec789c7758d160bf40f0
magicaljellybeans <tome.robin@gmail.com>
2020-08-02 14:27:39 +0100

    token auth for api

--- app/models.py
+++ app/models.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from hashlib import md5
 from time import time
 from flask import current_app, url_for
@@ -10,7 +10,8 @@ from app.search import add_to_index, remove_from_index, query_index
 import json
 import redis
 import rq
-
+import base64
+import os
 
 class PaginatedAPIMixin(object):
     @staticmethod
@@ -106,7 +107,9 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
     last_message_read_time = db.Column(db.DateTime)
     notifications = db.relationship('Notification', backref='user', lazy='dynamic')
     tasks = db.relationship('Task', backref='user', lazy='dynamic')
-    
+    token = db.Column(db.String(32), index=True, unique=True)
+    token_expiration = db.Column(db.DateTime)
+
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
@@ -207,6 +210,25 @@ class User(PaginatedAPIMixin, UserMixin, db.Model):
                 setattr(self, field, data[field])
         if new_user and 'password' in data:
             self.set_password(data['password'])
+
+    def get_token(self, expires_in=3600):
+        now = datetime.utcnow()
+        if self.token and self.token_expiration > now + timedelta(seconds=60):
+            return self.token
+        self.token = base64.b64encode(os.urandom(24)).decode('utf-8')
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.utcnow() - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = User.query.filter_by(token=token).first()
+        if user is None or user.token_expiration < datetime.utcnow():
+            return None
+        return user
 
 @login.user_loader
 def load_user(id):
